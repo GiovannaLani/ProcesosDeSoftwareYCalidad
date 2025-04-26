7a898554d636601ac4e4effad095d184d871d3b1
GiovannaLani <giovanna.lani@opendeusto.es>
2025-04-26 20:46:44 +0200

    test getItems

---
+++ vintedServer/src/test/java/com/spq/vinted/VintedApplicationTest.java
@@ -0,0 +1,11 @@
+package com.spq.vinted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VintedApplicationTest {
+    @Test
+	void contextLoads() {
+	}
+}
